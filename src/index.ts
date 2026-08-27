e776a73bb19d1806adebe5403695158d28187374
Kazutoyo Tokai <kazutoyo.jp@gmail.com>
2026-08-28 07:22:36 +0900

    feat: export the full-screen ad API

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,29 @@ export { useBannerAd, useBannerAdState } from './hooks/useBannerAd';
 export type { BannerAdState } from './hooks/useBannerAd';
 export { useBannerAdSize } from './hooks/useBannerAdSize';
 
+export { createInterstitialAd } from './InterstitialAd';
+export type { InterstitialAd, FullScreenAdEvents } from './InterstitialAd';
+
+export { createRewardedAd } from './RewardedAd';
+export type { RewardedAd, RewardedAdEvents } from './RewardedAd';
+
+export { ShowAdError } from './FullScreenAd';
+export type { FullScreenAdOptions } from './FullScreenAd';
+
+export { useInterstitialAd, useInterstitialAdState } from './hooks/useInterstitialAd';
+export type { FullScreenAdState } from './hooks/useInterstitialAd';
+export { useRewardedAd, useRewardedAdState } from './hooks/useRewardedAd';
+
 export type {
   AdError,
+  AdReward,
   AdapterResponse,
   BannerAdStatus,
+  FullScreenAdStatus,
   InitializationStatus,
   PaidEventValue,
   RequestConfiguration,
   RequestOptions,
   ResponseInfo,
+  ShowAdErrorCode,
 } from './types';
